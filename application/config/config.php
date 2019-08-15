f032405c8084e8ca9dbd792014b65c7519b4a649
Muhammad Maulana <maulana4real@gmail.com>
2019-08-15 16:52:26 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/monitoring';
+$config['base_url'] = 'http://localhost/hidroponik-web';
 
 /*
 |--------------------------------------------------------------------------
